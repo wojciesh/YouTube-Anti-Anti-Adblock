558ae84e0971254498a4a70617cd07342fa0d369
Wojciesh <wojciesh@gmail.com>
2023-10-19 23:06:49 +0200

    WIP

--- script.js
+++ script.js
@@ -46,44 +46,61 @@ function removePopup(popupContainer) {
   log('Removed popup container');
 }
 
-async function tryToResumeVideo() {
-  await resumeVideo();  // try to resume video immediately
+function tryToResumeVideo() {
+  resumeVideo();  // try to resume video immediately
   // Every 50ms until 500ms check if video is paused and try to resume it
   let loops = 0;
-  const interval = 50;
+  const limit = 20;
+  const interval = 100;
   const timer = setInterval(async () => {
     log(`Is video paused? (${++loops})`);
-    const video = document.querySelector('video');
-    if (!video) {
-      log('No video found!');
-    } else if (video.paused) {
-      log(`Video is paused, trying resuming again...`);
-      await resumeVideo();
-    } else {
-      log('Video is not paused');
-    }
-    if (loops >= 10) {
+    resumeVideo();
+    // const video = document.querySelector('video');    
+    // if (!video) {
+    //   log('Bad video!');
+    // } else if (video.paused === true) {
+    //   log(`Video is paused, trying resuming again...`);
+    //   await resumeVideo();
+    // } else {
+    //   log('Video is not paused');
+    // }
+    if (loops >= limit) {
       clearInterval(timer);
     }
   }, interval);
 
-  async function resumeVideo() {
+  function resumeVideo() {
+    log('Trying to resume video .......');
     const video = document.querySelector('video');
     try {
-      if (video?.paused) {
-        await video.play();
-        log('Video resumed');
+      // if (video?.paused === true) {
+
+        const playPromise = video.play();
+
+        if (playPromise !== undefined) {
+          playPromise.then(_ => {
+            log('Video resumed successfully');
+          })
+          .catch(error => {
+            log('NEW Error resuming video');
+            log(error.message);
+            console.error(error);
+          });
+        } else {
+          log('No play promise!');
+        }
+
         return video;
-      } else {
-        log('No paused video found');
-      }
+      // } else {
+      //   log('No paused video found');
+      // }
     } catch (err) {
       log('Error resuming video');
       log(err.message);
       console.error(err);
     }
     return null;
-  }  
+  }
 }
 
 function log(message) {
